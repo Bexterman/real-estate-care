204d0baa35501ed2d345d39fb4db8188c2a4726f
Ron <ronbexterman@live.nl>
2023-08-02 09:47:29 +0200

    added lazy loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,47 +1,37 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-
 import HomeView from '@/views/HomeView.vue';
-import AssignedView from '@/views/AssignedView.vue';
-import CompletedView from '@/views/CompletedView.vue';
-import KnowledgeBaseView from '@/views/KnowledgeBaseView.vue';
-import SettingsView from '@/views/SettingsView.vue';
-import NotificationsView from '@/views/NotificationsView.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home',
-  },
-  {
-    path: '/home',
-    name: 'Home',
+    name: 'home',
     component: HomeView
   },
   {
     path: '/assigned',
-    name: 'Assigned',
-    component: AssignedView
+    name: 'assigned',
+    component: () => import('@/views/AssignedView.vue')
   },
   {
     path: '/completed',
-    name: 'Completed',
-    component: CompletedView
+    name: 'completed',
+    component: () => import('@/views/CompletedView.vue')
   },
   {
     path: '/knowledgebase',
-    name: 'KnowledgeBase',
-    component: KnowledgeBaseView
+    name: 'knowledgebase',
+    component: () => import('@/views/KnowledgeBaseView.vue')
   },
   {
     path: '/settings',
-    name: 'Settings',
-    component: SettingsView
+    name: 'settings',
+    component: () => import('@/views/SettingsView.vue')
   },
   {
     path: '/notifications',
-    name: 'Notifications',
-    component: NotificationsView
+    name: 'notifications',
+    component: () => import('@/views/NotificationsView.vue')
   },
 ]
 
