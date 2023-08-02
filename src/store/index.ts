3fa9387bd0ac74ecb5be20a1ddd19959be0a3ba3
Ron <ronbexterman@live.nl>
2023-08-02 09:54:48 +0200

    created and edited store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,7 @@
 import { createStore } from "vuex";
 
-import damage from './modules/damage-store';
-import maintenance from './modules/maintenance-store';
-import technical from './modules/technical-store';
-import modifications from './modules/modifications-store';
 
 const store = createStore({
-  modules: {
-    damage,
-    maintenance,
-    technical,
-    modifications
-  },
 });
 
 export default store;
