40e293a5bed9692b9d92ffdfe5c2a1a468e8bada
Ron <ronbexterman@live.nl>
2023-08-02 09:46:13 +0200

    updated the router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
+import store from './store';
 
 import { IonicVue } from '@ionic/vue';
 
@@ -27,8 +28,9 @@ import PageLayout from './components/PageLayout.vue';
 
 
 const app = createApp(App)
-.use(IonicVue)
-.use(router);
+  .use(IonicVue)
+  .use(router)
+  .use(store);
 
 router.isReady().then(() => {
   app.mount('#app');
