5e311d0dc17339ebadf62023eacb8148a01fd942
Bexterman <ronbexterman@live.nl>
2023-09-16 16:04:29 +0200

    keep theme state

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,36 @@
 
 <script setup lang="ts">
 import { IonApp } from '@ionic/vue';
+import { ref, onMounted, watch } from 'vue';
 
+const darkTheme = ref(false);
 
+// Check local storage for dark theme preference when the component is mounted
+onMounted(() => {
+  const storedTheme = localStorage.getItem('darkTheme');
+  if (storedTheme === 'enabled') {
+    darkTheme.value = true;
+  }
+});
+
+// Watch for changes in dark theme and apply it to the entire app
+watch(darkTheme, (newValue: any) => {
+  const theme = document.getElementById('theme');
+  if (theme) {
+    if (newValue) {
+      theme.classList.add('darkTheme');
+    } else {
+      theme.classList.remove('darkTheme');
+    }
+  }
+});
 </script>
 
 <style>
 .slide-fade-enter-active {
   transition: all .4s ease;
 }
+
 .slide-fade-leave-active {
   transition: all 0s;
 }
